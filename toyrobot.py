0476250c0d9be069ee19fe7ae765164761a47d41
lilia-k <34472423+lilia-k@users.noreply.github.com>
2017-12-12 19:40:02 +1100

    Add files via upload
    Initial version

---
+++ toyrobot.py
@@ -0,0 +1,26 @@
+##############
+# Created on 12/12/2017
+# Version 001: Modified on 12/12/2017 by xxxxx
+# This is an application which enables the movement of a toy robot based on a set of action commands
+# These action commands are contained in a file, and should be placed in the folder C:\Python\input.txt
+###############
+
+# set variables
+
+# set valid movements/actions
+
+# Create dictionary of actions to determine function for an action
+
+# Create class function to determine outcome of action
+# create function for each move
+
+# open input file
+
+	# loop each line of the input file and perform action
+
+		# Check valid move
+		        # if action is a 'PLACE' movement, define the x and y position
+
+        # if action is a 'PLACE' movement, define the x and y position
+
+        # execute action commands 
